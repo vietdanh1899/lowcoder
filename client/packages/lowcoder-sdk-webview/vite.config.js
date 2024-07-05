9e69037bc28aac8e31f5ff220c34d41876757511
danhnv7 <danhnv7@viettel.com.vn>
2024-07-05 10:36:52 +0700

    revert: inline dynamic import

--- client/packages/lowcoder-sdk-webview/vite.config.js
+++ client/packages/lowcoder-sdk-webview/vite.config.js
@@ -5,11 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '',
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        inlineDynamicImports: true
-      },
-    },
-  },
 })
