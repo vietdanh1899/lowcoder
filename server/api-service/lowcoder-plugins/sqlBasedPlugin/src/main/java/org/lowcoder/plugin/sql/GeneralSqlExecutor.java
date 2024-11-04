a273dc2b514bd7bbd4154a63877718b2a07f1692
vietdanh1899 <nguyenvietdanhnph@gmail.com>
2024-11-04 08:45:26 +0700

    Revert "Fix null value error on mssql datasource"
    This reverts commit bec926da47821db94fa572f2494e3e28b9406e86.

--- server/api-service/lowcoder-plugins/sqlBasedPlugin/src/main/java/org/lowcoder/plugin/sql/GeneralSqlExecutor.java
+++ server/api-service/lowcoder-plugins/sqlBasedPlugin/src/main/java/org/lowcoder/plugin/sql/GeneralSqlExecutor.java
@@ -232,9 +232,7 @@ public class GeneralSqlExecutor {
 
     private void bindParam(int bindIndex, Object value, PreparedStatement preparedStatement, String bindKeyName) throws SQLException {
         if (value == null) {
-            ParameterMetaData parameterMetaData = preparedStatement.getParameterMetaData();
-            int paramType = parameterMetaData.getParameterType(bindIndex);
-            preparedStatement.setNull(bindIndex, paramType);
+            preparedStatement.setNull(bindIndex, Types.NULL);
             return;
         }
         if (value instanceof Integer intValue) {
