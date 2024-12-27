abea58ce940deadbbbc7811da4a06f8fd713fa90
danhnv7 <danhnv7@viettel.com.vn>
2024-12-27 11:50:25 +0700

    findDOMnode chart comps

--- client/packages/lowcoder-comps/src/comps/chartComp/chartComp.tsx
+++ client/packages/lowcoder-comps/src/comps/chartComp/chartComp.tsx
@@ -228,6 +228,7 @@ ChartTmpComp = withViewFn(ChartTmpComp, (comp) => {
           firstResize.current = false;
         }
       }}
+      targetRef={echartsCompRef}
     >
       {(mode !== 'map' || (mode === 'map' && isMapScriptLoaded)) && (
         <ReactECharts
