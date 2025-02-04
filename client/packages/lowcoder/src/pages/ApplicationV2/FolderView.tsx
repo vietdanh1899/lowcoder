1cdfdca1de48074aed7fa3b305ecac217e79de10
danhnv7 <danhnv7@viettel.com.vn>
2025-02-04 15:22:05 +0700

    folder view loading effect

--- client/packages/lowcoder/src/pages/ApplicationV2/FolderView.tsx
+++ client/packages/lowcoder/src/pages/ApplicationV2/FolderView.tsx
@@ -1,19 +1,30 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { HomeBreadcrumbType, HomeLayout } from "./HomeLayout";
-import {useEffect, useState} from "react";
-import {ApplicationCategoriesEnum, ApplicationMeta, FolderMeta} from "../../constants/applicationConstants";
+import { useEffect, useState } from "react";
+import {
+  ApplicationCategoriesEnum,
+  ApplicationMeta,
+  FolderMeta,
+} from "../../constants/applicationConstants";
 import { buildFolderUrl } from "../../constants/routesURL";
-import { folderElementsSelector, foldersSelector } from "../../redux/selectors/folderSelector";
+import {
+  folderElementsSelector,
+  foldersSelector,
+} from "../../redux/selectors/folderSelector";
 import { Helmet } from "react-helmet";
 import { trans } from "i18n";
-import {ApplicationPaginationType} from "@lowcoder-ee/util/pagination/type";
-import {fetchFolderElements} from "@lowcoder-ee/util/pagination/axios";
+import { ApplicationPaginationType } from "@lowcoder-ee/util/pagination/type";
+import { fetchFolderElements } from "@lowcoder-ee/util/pagination/axios";
+import {
+  ReduxActionErrorTypes,
+  ReduxActionTypes,
+} from "@lowcoder-ee/constants/reduxActionConstants";
 
 function getBreadcrumbs(
   folder: FolderMeta,
   allFolders: FolderMeta[],
-  breadcrumb: HomeBreadcrumbType[]
+  breadcrumb: HomeBreadcrumbType[],
 ): HomeBreadcrumbType[] {
   if (folder.parentFolderId) {
     return getBreadcrumbs(
@@ -25,7 +36,7 @@ function getBreadcrumbs(
           path: buildFolderUrl(folder.folderId),
         },
         ...breadcrumb,
-      ]
+      ],
     );
   }
   return breadcrumb;
@@ -39,14 +50,19 @@ interface ElementsState {
 export function FolderView() {
   const { folderId } = useParams<{ folderId: string }>();
 
-  const [elements, setElements] = useState<ElementsState>({ elements: [], total: 0 });
+  const [elements, setElements] = useState<ElementsState>({
+    elements: [],
+    total: 0,
+  });
   const [currentPage, setCurrentPage] = useState(1);
   const [pageSize, setPageSize] = useState(10);
   const [searchValues, setSearchValues] = useState("");
   const [typeFilter, setTypeFilter] = useState<number>(0);
   const [modify, setModify] = useState(true);
   const [searchValue, setSearchValue] = useState("");
-  const [categoryFilter, setCategoryFilter] = useState<ApplicationCategoriesEnum | "All">("All");
+  const [categoryFilter, setCategoryFilter] = useState<
+    ApplicationCategoriesEnum | "All"
+  >("All");
 
   const dispatch = useDispatch();
 
@@ -61,61 +77,67 @@ export function FolderView() {
     },
   ]);
 
-  useEffect( () => {
-        try{
-          fetchFolderElements({
-            id: folderId,
-            pageNum:currentPage,
-            pageSize:pageSize,
-            applicationType: ApplicationPaginationType[typeFilter],
-            name: searchValues,
-            category: categoryFilter === "All" ? "" : categoryFilter
-          }).then(
-              (data: any) => {
-                if (data.success) {
-                  setElements({elements: data.data || [], total: data.total || 1})
-                }
-                else
-                  console.error("ERROR: fetchFolderElements", data.error)
-              }
-          );
-        } catch (error) {
-          console.error('Failed to fetch data:', error);
-        }
-      }, [currentPage, pageSize, searchValues, typeFilter, modify, categoryFilter]);
+  useEffect(() => {
+    try {
+      dispatch({
+        type: ReduxActionTypes.FETCH_FOLDER_ELEMENTS_INIT,
+        payload: {},
+      });
+      fetchFolderElements({
+        id: folderId,
+        pageNum: currentPage,
+        pageSize: pageSize,
+        applicationType: ApplicationPaginationType[typeFilter],
+        name: searchValues,
+        category: categoryFilter === "All" ? "" : categoryFilter,
+      }).then((data: any) => {
+        dispatch({
+          type: ReduxActionTypes.FETCH_FOLDER_ELEMENTS_SUCCESS,
+          payload: {},
+        });
+        if (data.success) {
+          setElements({ elements: data.data || [], total: data.total || 1 });
+        } else console.error("ERROR: fetchFolderElements", data.error);
+      });
+    } catch (error) {
+      dispatch({
+        type: ReduxActionErrorTypes.FETCH_FOLDER_ELEMENTS_ERROR,
+        payload: {},
+      });
+      console.error("Failed to fetch data:", error);
+    }
+  }, [currentPage, pageSize, searchValues, typeFilter, modify, categoryFilter]);
 
-    useEffect( () => {
-            if (searchValues !== "")
-                setCurrentPage(1);
-        }, [searchValues]
-    );
+  useEffect(() => {
+    if (searchValues !== "") setCurrentPage(1);
+  }, [searchValues]);
 
-    useEffect(()=> {
-        const timer = setTimeout(() => {
-            if (searchValue.length > 2 || searchValue === "")
-                setSearchValues(searchValue)
-        }, 500);
-        return () => clearTimeout(timer);
-    }, [searchValue])
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (searchValue.length > 2 || searchValue === "")
+        setSearchValues(searchValue);
+    }, 500);
+    return () => clearTimeout(timer);
+  }, [searchValue]);
 
   return (
     <>
       <Helmet>{<title>{trans("home.yourFolders")}</title>}</Helmet>
       <HomeLayout
-          elements={elements.elements}
-          mode={"folder"}
-          breadcrumb={breadcrumbs}
-          currentPage ={currentPage}
-          setCurrentPage={setCurrentPage}
-          pageSize={pageSize}
-          setPageSize={setPageSize}
-          total={elements.total}
-          setSearchValue={setSearchValue}
-          searchValue={searchValue}
-          setTypeFilterPagination={setTypeFilter}
-          setModify={setModify}
-          modify={modify}
-          setCategoryFilterPagination={setCategoryFilter}
+        elements={elements.elements}
+        mode={"folder"}
+        breadcrumb={breadcrumbs}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        pageSize={pageSize}
+        setPageSize={setPageSize}
+        total={elements.total}
+        setSearchValue={setSearchValue}
+        searchValue={searchValue}
+        setTypeFilterPagination={setTypeFilter}
+        setModify={setModify}
+        modify={modify}
+        setCategoryFilterPagination={setCategoryFilter}
       />
     </>
   );
--- client/packages/lowcoder/src/pages/ApplicationV2/HomeView.tsx
+++ client/packages/lowcoder/src/pages/ApplicationV2/HomeView.tsx
@@ -1,65 +1,92 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { HomeLayout } from "./HomeLayout";
 import { getUser } from "../../redux/selectors/usersSelectors";
 import { Helmet } from "react-helmet";
 import { trans } from "i18n";
-import {useState, useEffect } from "react";
-import {fetchFolderElements} from "@lowcoder-ee/util/pagination/axios";
-import {ApplicationCategoriesEnum, ApplicationMeta, FolderMeta} from "@lowcoder-ee/constants/applicationConstants";
-import {ApplicationPaginationType} from "@lowcoder-ee/util/pagination/type";
+import { useState, useEffect } from "react";
+import { fetchFolderElements } from "@lowcoder-ee/util/pagination/axios";
+import {
+  ApplicationCategoriesEnum,
+  ApplicationMeta,
+  FolderMeta,
+} from "@lowcoder-ee/constants/applicationConstants";
+import { ApplicationPaginationType } from "@lowcoder-ee/util/pagination/type";
+import {
+  ReduxActionErrorTypes,
+  ReduxActionTypes,
+} from "@lowcoder-ee/constants/reduxActionConstants";
 
 interface ElementsState {
-    elements: (ApplicationMeta | FolderMeta)[];
-    total: number;
+  elements: (ApplicationMeta | FolderMeta)[];
+  total: number;
 }
 
 export function HomeView() {
-    const [elements, setElements] = useState<ElementsState>({ elements: [], total: 1 });
-    const [currentPage, setCurrentPage] = useState(1);
-    const [pageSize, setPageSize] = useState(10);
-    const [searchValue, setSearchValue] = useState("");
-    const [searchValues, setSearchValues] = useState("");
-    const [typeFilter, setTypeFilter] = useState<number>(0);
-    const [modify, setModify] = useState(true);
-    const [isCreated, setIsCreated] = useState(true);
-    const [categoryFilter, setCategoryFilter] = useState<ApplicationCategoriesEnum | "All">("All");
+  const [elements, setElements] = useState<ElementsState>({
+    elements: [],
+    total: 1,
+  });
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize, setPageSize] = useState(10);
+  const [searchValue, setSearchValue] = useState("");
+  const [searchValues, setSearchValues] = useState("");
+  const [typeFilter, setTypeFilter] = useState<number>(0);
+  const [modify, setModify] = useState(true);
+  const [isCreated, setIsCreated] = useState(true);
+  const [categoryFilter, setCategoryFilter] = useState<
+    ApplicationCategoriesEnum | "All"
+  >("All");
+  const dispatch = useDispatch();
 
-      useEffect( () => {
-        try{
-          fetchFolderElements({
-            pageNum:currentPage,
-            pageSize:pageSize,
-            applicationType: ApplicationPaginationType[typeFilter],
-            name: searchValues,
-            category: categoryFilter === "All" ? "" : categoryFilter
-          }).then(
-            (data: any) => {
-              if (data.success) {
-                setElements({elements: data.data || [], total: data.total || 1})
-              }
-              else
-                console.error("ERROR: fetchFolderElements", data.error)
-            }
-          );
-        } catch (error) {
-            console.error('Failed to fetch data:', error);
-        }
-        }, [currentPage, pageSize, searchValues, typeFilter, modify, categoryFilter, isCreated]
-      );
+  useEffect(() => {
+    try {
+      dispatch({
+        type: ReduxActionTypes.FETCH_FOLDER_ELEMENTS_INIT,
+        payload: {},
+      });
+      fetchFolderElements({
+        pageNum: currentPage,
+        pageSize: pageSize,
+        applicationType: ApplicationPaginationType[typeFilter],
+        name: searchValues,
+        category: categoryFilter === "All" ? "" : categoryFilter,
+      }).then((data: any) => {
+        dispatch({
+          type: ReduxActionTypes.FETCH_FOLDER_ELEMENTS_SUCCESS,
+          payload: {},
+        });
+        if (data.success) {
+          setElements({ elements: data.data || [], total: data.total || 1 });
+        } else console.error("ERROR: fetchFolderElements", data.error);
+      });
+    } catch (error) {
+      dispatch({
+        type: ReduxActionErrorTypes.FETCH_FOLDER_ELEMENTS_ERROR,
+        payload: {},
+      });
+      console.error("Failed to fetch data:", error);
+    }
+  }, [
+    currentPage,
+    pageSize,
+    searchValues,
+    typeFilter,
+    modify,
+    categoryFilter,
+    isCreated,
+  ]);
 
-    useEffect( () => {
-            if (searchValues !== "")
-                setCurrentPage(1);
-        }, [searchValues]
-    );
+  useEffect(() => {
+    if (searchValues !== "") setCurrentPage(1);
+  }, [searchValues]);
 
-    useEffect(()=> {
-        const timer = setTimeout(() => {
-            if (searchValue.length > 2 || searchValue === "")
-                setSearchValues(searchValue)
-        }, 500);
-        return () => clearTimeout(timer);
-    }, [searchValue])
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (searchValue.length > 2 || searchValue === "")
+        setSearchValues(searchValue);
+    }, 500);
+    return () => clearTimeout(timer);
+  }, [searchValue]);
 
   const user = useSelector(getUser);
 
@@ -69,11 +96,17 @@ export function HomeView() {
 
   return (
     <>
-      <Helmet>{<title>{trans("productName")} {trans("home.home")}</title>}</Helmet>
+      <Helmet>
+        {
+          <title>
+            {trans("productName")} {trans("home.home")}
+          </title>
+        }
+      </Helmet>
       <HomeLayout
         elements={elements.elements}
         mode={"view"}
-        currentPage ={currentPage}
+        currentPage={currentPage}
         setCurrentPage={setCurrentPage}
         pageSize={pageSize}
         setPageSize={setPageSize}
