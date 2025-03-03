bede0af19fad7116321ae1f408b073209eb139ad
danhnv7 <danhnv7@viettel.com.vn>
2025-03-03 11:44:38 +0700

    JSONValue undefined

--- client/packages/lowcoder-core/src/util/jsonTypes.ts
+++ client/packages/lowcoder-core/src/util/jsonTypes.ts
@@ -1,4 +1,4 @@
-export type JSONValue = string | number | boolean | JSONObject | JSONArray | null;
+export type JSONValue = string | number | boolean | JSONObject | JSONArray | null | undefined;
 
 export interface JSONObject {
   [x: string]: JSONValue | undefined;
