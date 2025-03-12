b90574821cb4335d12f5b73d47ed44462ce76584
danhnv7 <danhnv7@viettel.com.vn>
2025-03-12 16:35:16 +0700

    mount micro app

--- client/packages/lowcoder-sdk-webview/src/main.tsx
+++ client/packages/lowcoder-sdk-webview/src/main.tsx
@@ -1,10 +1,17 @@
 import React from "react";
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom/client";
 import App from "./App";
+import { AppMetadata, mountMicroApp, registerMicroApps } from "./micro-frontend";
+import microAppConfigs from "./micro-apps.json";
+
+(window as any).registerMicroApps = registerMicroApps;
+(window as any).mountMicroApp = mountMicroApp;
+
+registerMicroApps(microAppConfigs as AppMetadata[]);
 
 const apiKey = new URL(location.href).searchParams.get("LOWCODER_API_KEY");
 if (apiKey) {
-    localStorage.setItem("LOWCODER_API_KEY", apiKey);
+  localStorage.setItem("LOWCODER_API_KEY", apiKey);
 }
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App/>);
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<App />);
